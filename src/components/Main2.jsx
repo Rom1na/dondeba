ab3c41a60c01abed1d9e3ddb8fb46838fcb94b9e
Rom1na <romina.lc.perez@gmail.com>
2024-04-23 01:24:21 -0300

    Modificacion Main4

--- src/components/Main2.jsx
+++ src/components/Main2.jsx
@@ -15,7 +15,7 @@ const Main = () => {
     
     const barrios = sel.barrios;
     const tipos = sel.tipos;
-    
+    const texto = farmacias[0].barrio;
     
     
     
@@ -173,7 +173,7 @@ const Main = () => {
                  }
                onChange={manejarSelectTipo}> 
                  
-                         <option selected>¿Qué buscás?{farmacias[0].objeto}</option>     
+                         <option selected>¿Qué buscás?{texto}</option>     
                          {
                              tipos?.map((valor)=>(
                                <option key={valor} value={valor}>

9b3748c4bc257e056d0275db041ac67b099c651b
Rom1na <romina.lc.perez@gmail.com>
2024-04-23 21:48:54 -0300

    fondo botones bottom

--- src/components/Contenedor.jsx
+++ src/components/Contenedor.jsx
@@ -189,7 +189,8 @@ const Contenedor = () => {
     
     style={{
       marginRight:"5px",
-      fontSize:"11px"}         
+      fontSize:"11px",
+      backgroundColor:"black" }         
     }
     onClick={disminucion}
 
@@ -198,7 +199,8 @@ const Contenedor = () => {
 { !(data.length <= pag.fin) &&<button
     style={{
       marginRight:"5px",
-      fontSize:"11px"}         
+      fontSize:"11px",
+      backgroundColor: "black"}         
     }
     onClick={aumento}
 
